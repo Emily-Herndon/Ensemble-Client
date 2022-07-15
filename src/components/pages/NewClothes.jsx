53872d9354f530bf6c369b423cc81603f9dbf342
Emily-Herndon <emilyvherndon@gmail.com>
2022-07-14 21:10:37 -0700

    started new clothes form

--- src/components/pages/NewClothes.jsx
+++ src/components/pages/NewClothes.jsx
@@ -4,6 +4,13 @@ export default function NewClothes() {
 	return (
 		<div>
 			NewClothes
+			<form>
+				<input 
+				id="clothesName"
+				type="text"
+				placeholder="Clothes Name"
+				/>
+			</form>
 		</div>
 	)
 }
